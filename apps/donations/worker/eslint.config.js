7ab3477ef3575e0c760d060e1c28d3552f8a64ce
alok <alok0@users.noreply.github.com>
2026-06-08 09:48:04 -0400

    Enable stricter eslint rules

--- apps/donations/worker/eslint.config.js
+++ apps/donations/worker/eslint.config.js
@@ -81,4 +81,20 @@ export default tseslint.config(
       ],
     },
   },
+  {
+    ignores: ["**/__tests__/*"],
+    rules: {
+      "@typescript-eslint/no-unsafe-argument": "warn",
+      "@typescript-eslint/no-unsafe-assignment": "warn",
+      "@typescript-eslint/no-unsafe-call": "warn",
+      "@typescript-eslint/no-unsafe-declaration-merging": "warn",
+      "@typescript-eslint/no-unsafe-enum-comparison": "warn",
+      "@typescript-eslint/no-unsafe-function-type": "warn",
+      "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/no-unsafe-return": "warn",
+      "@typescript-eslint/no-unsafe-type-assertion": "warn",
+      "@typescript-eslint/no-unsafe-unary-minus": "warn",
+      "@typescript-eslint/no-non-null-assertion": "warn",
+    },
+  },
 );
